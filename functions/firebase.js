a816b571f7c79ad779c2f6bc09f36008ebced4a5
Agatha Floreta <150997841+andieyey@users.noreply.github.com>
2025-03-18 10:05:38 +0800

    Update firebase.js

--- functions/firebase.js
+++ functions/firebase.js
@@ -3,17 +3,19 @@ import { getFirestore, collection, getDocs, addDoc, serverTimestamp } from 'fire
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsV81XXaoR4GVtjrCvqnj7HN0DSEcCMNc",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "pizza-sales-tracker.firebaseapp.com",
   projectId: "pizza-sales-tracker",
-  storageBucket: "pizza-sales-tracker.firebasestorage.app",
+  storageBucket: "pizza-sales-tracker.appspot.com",
   messagingSenderId: "95670003992",
   appId: "1:95670003992:web:e8e6d6806c7ba999fd166d",
   measurementId: "G-2D49Z3Z06M"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app); 
 
 export { db, collection, getDocs, addDoc, serverTimestamp };  
+
