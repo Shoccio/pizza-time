c45b087234c5d4e220493be495abceebfb845342
Agatha Floreta <150997841+andieyey@users.noreply.github.com>
2025-03-18 09:39:53 +0800

    Create firebase.js

---
+++ functions/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, addDoc, serverTimestamp } from 'firebase/firestore'; 
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsV81XXaoR4GVtjrCvqnj7HN0DSEcCMNc",
+  authDomain: "pizza-sales-tracker.firebaseapp.com",
+  projectId: "pizza-sales-tracker",
+  storageBucket: "pizza-sales-tracker.firebasestorage.app",
+  messagingSenderId: "95670003992",
+  appId: "1:95670003992:web:e8e6d6806c7ba999fd166d",
+  measurementId: "G-2D49Z3Z06M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); 
+
+export { db, collection, getDocs, addDoc, serverTimestamp };  
