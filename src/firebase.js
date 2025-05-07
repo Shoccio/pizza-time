55ac24c0edf5941cc1de1772677296fa65338ce9
DevHiroImamura <151002489+DevHiroImamura@users.noreply.github.com>
2025-05-07 15:47:10 +0800

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs, addDoc, serverTimestamp } from 'firebase/firestore'; 
-
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, push, get, onValue, serverTimestamp } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDNPEKXKVleCJlst3eErmS12-WUK3xEGyQ",
   authDomain: "pizza-tracker-7a851.firebaseapp.com",
+  databaseURL: "https://pizza-tracker-7a851-default-rtdb.firebaseio.com",
   projectId: "pizza-tracker-7a851",
-  storageBucket: "pizza-tracker-7a851.firebasestorage.app",
+  storageBucket: "pizza-tracker-7a851.appspot.com", // ✅ fixed
   messagingSenderId: "118026448010",
-  appId: "1:118026448010:web:a5db0044563fff223f9173",
-  measurementId: "G-YC5P9X60HH"
+  appId: "1:118026448010:web:507de6e64a0a873c3f9173",
+  measurementId: "G-DCFRT30P1S"
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); 
+const db = getDatabase(app);
 
-export { db, collection, getDocs, addDoc, serverTimestamp };  
-
+export { db, ref, push, get, onValue, serverTimestamp };
